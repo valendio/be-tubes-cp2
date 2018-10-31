9fa382f0d718d6cdce919d53e325b09ef88587c1
alif.nurdianto <alif.nurdianto@helio.id>
2018-10-31 16:51:40 +0700

    Create unit test

---
+++ test/unit/modules/user/repositories/commands/command_handler_test.js
@@ -0,0 +1,32 @@
+const commandHandler = require('../../../../../../bin/modules/user/repositories/commands/command_handler');
+const User = require('../../../../../../bin/modules/user/repositories/commands/domain');
+const sinon = require('sinon');
+const assert = require('assert');
+
+describe('User-commandHandler', () => {
+
+  describe('postDataLogin', () => {
+    const data = {
+      success: true,
+      data: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9',
+      message: 'Your Request Has Been Processed',
+      code: 200
+    };
+
+    const payload = {
+      'username': 'alifsn',
+      'password': 'telkomdev'
+    };
+
+    it('should return access token', async() => {
+      sinon.stub(User.prototype, 'generateCredential').resolves(data);
+
+      const rs = await commandHandler.postDataLogin(payload);
+
+      assert.notEqual(rs.data, null);
+      assert.equal(rs.code, 200);
+
+      User.prototype.generateCredential.restore();
+    });
+  });
+});
