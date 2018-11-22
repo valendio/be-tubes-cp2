0b7f3296fcedbd18ff09a664d80b815b5fc3a99b
alif.nurdianto <alif.nurdianto@helio.id>
2018-11-22 10:58:50 +0700

    Add unit test command.js

--- test/unit/modules/user/repositories/commands/command_test.js
+++ test/unit/modules/user/repositories/commands/command_test.js
@@ -6,4 +6,23 @@ const Mongo = require('../../../../../../bin/helpers/databases/mongodb/db');
 
 describe('User-command', () => {
 
+  describe('insertOneUser', () => {
+    const queryResult = {
+      'err': null,
+      'data': {
+        '_id': '5bac53b45ea76b1e9bd58e1c',
+        'username': 'alifsndev',
+        'password': '8789ad457ac341e4fc4cad32'
+      },
+      'message': 'Your Request Has Been Processed',
+      'code': 200
+    };
+
+    it('should success to insert data to db', async() => {
+      sinon.stub(Mongo.prototype, 'insertOne').resolves(queryResult);
+      const res = await command.insertOneUser({});
+      assert.equal(res.data.username, queryResult.data.username);
+    });
+  });
+
 });
