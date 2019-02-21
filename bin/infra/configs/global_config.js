c4041e52a342f96f364557e61f336667e7db68eb
alif.nurdianto <alif.nurdianto@helio.id>
2019-02-21 16:03:01 +0700

    create helper postgres

--- bin/infra/configs/global_config.js
+++ bin/infra/configs/global_config.js
@@ -33,6 +33,16 @@ const config = {
     ],
     maxRetries: process.env.ELASTICSEARCH_MAX_RETRIES,
     requestTimeout: process.env.ELASTICSEARCH_REQUEST_TIMEOUT
+  },
+  postgreConfig:{
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    port: process.env.POSTGRES_PORT,
+    max:  parseInt(process.env.POSTGRES_MAX),
+    idleTimeoutMillis: parseInt(process.env.POSTGRES_TIMEOUT)
+
   }
 };
 
