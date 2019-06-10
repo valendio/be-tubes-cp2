7178485e0552e5c914c88886bdb1ad4d04bc1f62
alif.nurdianto <alif.nurdianto@helio.id>
2019-06-10 17:28:22 +0700

    add unit test query user

--- test/unit/modules/user/repositories/queries/domain_test.js
+++ test/unit/modules/user/repositories/queries/domain_test.js
@@ -1,31 +1,48 @@
-// const User = require('../../../../../../bin/modules/user/repositories/queries/domain');
-// const query = require('../../../../../../bin/modules/user/repositories/queries/query');
-// const sinon = require('sinon');
-// const assert = require('assert');
+const User = require('../../../../../../bin/modules/user/repositories/queries/domain');
+const query = require('../../../../../../bin/modules/user/repositories/queries/query');
+const sinon = require('sinon');
+const assert = require('assert');
 
 
-// describe('viewUser', () => {
+describe('viewUser', () => {
 
-//   it('should return user data', async() => {
+  it('should return user data', async() => {
 
-//     let queryResult = {
-//       'err': null,
-//       'data': {
-//         '_id': '5bac53b45ea76b1e9bd58e1c',
-//         'username': 'alifsndev',
-//         'password': '8789ad457ac341e4fc4cad32'
-//       },
-//       'message': 'Your Request Has Been Processed',
-//       'code': 200
-//     };
+    let queryResult = {
+      'err': null,
+      'data': {
+        '_id': '5bac53b45ea76b1e9bd58e1c',
+        'username': 'alifsndev',
+        'password': '8789ad457ac341e4fc4cad32'
+      }
+    };
 
-//     sinon.stub(query, 'findById').resolves(queryResult);
+    sinon.stub(query.prototype, 'findById').resolves(queryResult);
 
-//     const userId = '5bac53b45ea76b1e9bd58e1c';
-//     const user = new User();
-//     const result = await user.viewUser(userId);
+    const userId = '5bac53b45ea76b1e9bd58e1c';
+    const user = new User();
+    const result = await user.viewUser(userId);
 
-//     assert.equal(result.code, 200);
+    query.prototype.findById.restore();
+    assert.equal(result.data.username, 'alifsndev');
 
-//   });
-// });
+  });
+
+  it('should return error', async() => {
+
+    let queryResult = {
+      'err': true,
+      'data': null
+    };
+
+    sinon.stub(query.prototype, 'findById').resolves(queryResult);
+
+    const userId = '5bac53b45ea76b1e9bd58e1c';
+    const user = new User();
+    const result = await user.viewUser(userId);
+
+    query.prototype.findById.restore();
+    assert.equal(result.err, 'Can not find user');
+
+  });
+});
